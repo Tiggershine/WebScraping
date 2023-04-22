b20d406fcf5557a301f6cd24aeca715ca2d5bf70
Hyun Kim <kmasterh@gmail.com>
2023-04-22 19:45:24 +0200

    "Setting weekend infomation in the mensa menu"

--- app.py
+++ app.py
@@ -10,53 +10,53 @@ import time
 
 app = Flask(__name__)
 
+# Set webdrive option
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
  
-# Chrome 드라이버 생성
+# Generate Chrome driver 
 browser1 = webdriver.Chrome(options = options)
 browser2 = webdriver.Chrome(options = options)
 
 
-# 요일 List (number got from datetime library)
-weekday = { 0: 'Montag', 1: 'Dienstag', 2: 'Mittwoch', 3: 'Donnerstag', 4: 'Freitag' }
+# Weekday List (number got from datetime library)
+weekday = { 0: 'Montag', 1: 'Dienstag', 2: 'Mittwoch', 3: 'Donnerstag', 4: 'Freitag', 5: 'Montag', 6: 'Montag' }
 
 
-# pollen 종류
+# pollen type
 pollenType = [ 'Ambrosia', 'Ampfer', 'Beifuß', 'Buche', 'Erle', 'Esche', 'Gräser', 'Hasel', 'Pappel', 'Roggen', 'Ulme', 'Wegerich', 'Weide' ]
-# pollen 세기
+# pollen intensity
 burdenType = { 'noburden': 0, 'weakburden': 1, 'moderateburden': 2, 'strongburden': 3 }
-# pollen 종류-세기 딕셔너리 
+# pollen type-intensity dictionary
 pollenList = {'Ambrosia': '', 'Ampfer': '', 'Beifuß': '', 'Buche': '', 'Erle': '', 'Esche': '', 'Gräser': '', 'Hasel': '', 'Pappel': '', 'Roggen': '', 'Ulme': '', 'Wegerich': '', 'Weide': ''} 
 
 
 @app.route('/')
 def menu():
   # [ Mensa Today's Menu ]
-  # Mensa 페이지 열기
+  # Open Mensa website  
   browser1.get('https://www.studierendenwerk-aachen.de/speiseplaene/academica-w.html')
 
   today = weekday[datetime.datetime.today().weekday()]
 
-  todayElement = browser1.find_element(By.ID, '{}'.format(today))  # 검색 결과 가져오기
-  menus = todayElement.find_elements(By.CLASS_NAME, 'menue-wrapper')
+  # get Element for today's menu
+  if datetime.datetime.today().weekday() == 5 or datetime.datetime.today().weekday() == 6:
+    menus = "Not open"
+  else:
+    todayElement = browser1.find_element(By.ID, '{}'.format(today))
+    menus = todayElement.find_elements(By.CLASS_NAME, 'menue-wrapper')
 
-  # todayElement = wait.until(EC.presence_of_element_located((By.ID, '{}'.format(today))))
-  # menus = todayElement.find_elements(By.CLASS_NAME, 'menue-wrapper')
-
-
-  # time.sleep(30)
 
   # [ Today's Pollen - Aachen ]
+  # Using BeautifulSoup library, since was having trouble getting the weather elements
   browser2.get('https://www.wetteronline.de/pollenvorhersage?gid=10501&lat=50.783&locationname=Aachen&lon=6.083')
-  # time.sleep(20)
+
   html = browser2.page_source
   soup = BeautifulSoup(html, 'html.parser')
   names = soup.find_all(class_='polle_text')
-
 
   for name in names:
     next_sibling = name.find_next_sibling('div') # 'div' 태그를 가진 sibling 찾기
